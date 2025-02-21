ef9a75173becad2f318f977eb51a6189c0bb10b9
Haris <harisaans@gmail.com>
2025-02-21 17:59:37 +0530

    added pipeline and fixed typo

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -9,7 +9,7 @@ const Intro = () => {
     <section id="intro">
         <div class="introContent">
             <span class="hello">Hello, </span>
-            <span class="introText">I'm <span class="introName">Haris</span><br/>Webiste Designer</span>
+            <span class="introText">I'm <span class="introName">Haris</span><br/>Website Designer</span>
             <p class="introPara">I am a skilled and passionate web designer with experience in<br/> creating visually appealing and user friendly websites.</p>
             <Link><button class="btn"><img src={btnImg} alt="" className="btnImg"/>Hire Me!</button></Link>
 
